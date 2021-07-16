d3ae1859d1bea0113b8f57194a43f27213b8bc40
avyukd <avyukd@gmail.com>
2021-07-15 20:12:00 -0400

    Added Flask backend. Completed Award Card component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,8 @@ const ENTITY_SEARCH_URL = "https://api.sam.gov/entity-information/v2/entities";
 
 const SBA_SEARCH_URL = "https://web.sba.gov/pro-net/search/dsp_profile.cfm";
 
+const COMPANYINFO_URL = "http://127.0.0.1:5000/api/company_info"
+
 async function searchCallback(response){
 
 }
@@ -20,47 +22,45 @@ const App = () => {
 
   const [searchResponseObj, setSearchResponseObj] = useState({"results":[]});
   const [filterDataObj, setFilterDataObj] = useState({});
+  const [loading, setLoading] = useState(false);
 
   const retrieveFilter = data => {
     console.log(data)
     request_default_obj.filters.keywords = data.keywords.split(", ");
     const newRDO = {...request_default_obj}
     setFilterDataObj(newRDO);
+    setLoading(true);
   }
   //request for sba example: https://web.sba.gov/pro-net/search/dsp_profile.cfm?DUNS=831761957
+  const maxResults = 5;
+  let cnt = 0;
   useEffect(() => {
     if(filterDataObj != {}){
-      console.log("In useEffect!")
       axios.post(AWARD_SEARCH_REQUEST_URL, filterDataObj).then(async (response) => {
-        
+        let filteredAwards = {"results":[]}
         for(const award of response.data.results){
           const DUNS = award["Recipient DUNS Number"];
-          
-          const SAM_entity_search_response = await axios.get(ENTITY_SEARCH_URL, {
+
+          const Company_info_response = await axios.get(COMPANYINFO_URL, {
             params : {
-              "api_key" : keys.SAM_API_KEY,
-              "ueiDUNS" : DUNS
+              "duns" : DUNS,
+              "api_key": keys.SAM_API_KEY
             }
           })
-
-          /*const SBA_entity_search_response = await axios.get(SBA_SEARCH_URL, {
-            headers: {
-              'Content-Type': 'application/x-www-form-urlencoded',
-              'Access-Control-Allow-Origin': '*'
-            },
-            params : {
-              "DUNS" : DUNS
-            }
-          })*/
-
-          //console.log(SBA_entity_search_response);
           
-          award["SAM Company Information"] = SAM_entity_search_response.data.entityData;
-          award["SBA Company Information"] = {link : SBA_SEARCH_URL+"?DUNS="+DUNS}
-          //SBA_entity_search_response;
+          console.log("Response data", Company_info_response.data);
+          award["Company Information"] = Company_info_response.data;
+          if(Object.keys(Company_info_response.data).length !== 0){
+            filteredAwards.results.push(award);
+          }
+          cnt++;
+          if(cnt >= maxResults){
+            break;
+          }
         }
-        console.log(response.data);
-        setSearchResponseObj(response.data);
+        //console.log(response.data);
+        setSearchResponseObj(filteredAwards);
+        setLoading(false);
       })
     }
     
@@ -74,9 +74,12 @@ const App = () => {
         </Container>
         <Container>
           {
+            loading ? 
+            "Loading..." :
+            searchResponseObj.results.length > 0 ?
             searchResponseObj.results.map((el) =>
               (<div><AwardCard awardInfo={el}/></div>)
-            )
+            ) : "No results."
           }
         </Container>
       </Container>
---
+++ frontend/src/AwardCard.js
@@ -0,0 +1,84 @@
+import { Typography, Card, CardContent, Container } from "@material-ui/core";
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+//props contains one object: awardInfo
+//all elements accessed using props.awardInfo['COL_NAME']
+const urlValidate = url => {
+    if(url.substring(0,4)!=="http"){
+        return "http://"+url;
+    }else{
+        return url; 
+    }
+}
+
+const AwardCard = props => {
+
+    return (
+        <>
+            <Card>
+                <CardContent>
+                    <Typography variant="h5"><a href={urlValidate(props.awardInfo["Company Information"].url)}>
+                        {props.awardInfo["Recipient Name"]}</a></Typography>
+                    <Typography> <b>Award Amount:</b> ${Number(props.awardInfo["Award Amount"]).toLocaleString()}</Typography>
+                    <Typography> <b>Agency:</b> {props.awardInfo["Awarding Agency"]}, {props.awardInfo["Awarding Sub Agency"]}</Typography>
+                    <Typography><b>Period of Performance:</b> {props.awardInfo["Start Date"]} to {props.awardInfo["End Date"]}</Typography>
+                    <Typography><b>Award ID:</b> {props.awardInfo["Award ID"]}</Typography>
+                    <Typography><b>Description:</b> {props.awardInfo["Description"].substring(0,250)}</Typography>
+                <Accordion>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}>
+                            <Typography>Company Details</Typography>
+                        </AccordionSummary>
+                    <AccordionDetails>
+                        <Container>
+                            <Typography><b>Socio-economic Status:</b> {props.awardInfo["Company Information"]["socio_economic_status"].join(", ")}</Typography>
+                            <Typography><b>Primary NAICS:</b> {props.awardInfo["Company Information"]["primary_NAICS"]}</Typography>
+                            <Typography><b>Point of Contact:</b> {props.awardInfo["Company Information"]["Contact Person:"]}</Typography>
+                            <Typography><b>POC Email:</b> {props.awardInfo["Company Information"]["E-mail Address:"]}</Typography>
+                            <Typography><b>Capabilities Narrative:</b> {props.awardInfo["Company Information"]["Capabilities Narrative"]}</Typography>
+                            <Typography><b>GSA Contract Number:</b> {props.awardInfo["Company Information"]["GSA Advantage Contract(s):"]}</Typography>
+                            <Typography><b>8(a) Start:</b> {props.awardInfo["Company Information"]["sba_8a_entrance"]}</Typography>
+                            <Typography><b>8(a) End:</b> {props.awardInfo["Company Information"]["sba_8a_exit"]}</Typography>
+                        </Container>
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion>
+                        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography>Past Performance</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Container>
+                            {props.awardInfo["Company Information"]["References"].map(
+                                (el) => {
+                                    return (
+                                        <>
+                                            <Typography variant="h6"><b>Contract Name:</b> {el["Contract:"]}</Typography>
+                                            <Typography><b>Agency:</b> {el["Name:"]}</Typography>
+                                            <Typography><b>Start:</b> {el["Start:"]}</Typography>
+                                            <Typography><b>End:</b> {el["End:"]}</Typography>
+                                            <Typography><b>Value:</b> {el["Value:"]}</Typography>
+                                            <Typography><b>Contact:</b> {el["Contact:"]}</Typography>
+                                            <Typography><b>Phone:</b> {el["Phone:"]}</Typography>
+                                        </>
+                                    );
+                                }
+                            )}
+                            </Container>
+                        </AccordionDetails>
+                </Accordion>
+                </CardContent>
+            </Card>
+        </>
+    );
+}
+
+export default AwardCard;
+
+/*  {
+        (props.awardInfo["SAM Company Information"] !== undefined) &&
+        getPointsOfContactJSX(props.awardInfo["SAM Company Information"][0].pointsOfContact)
+    }
+*/
